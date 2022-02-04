690762dc6c86276a40c822feeecf1e311ff946fc
Kaio Garcia <contatokaiogarcia@gmail.com>
2022-02-04 09:17:00 -0300

    Finalizando Codigo.

--- front/pro-atividade-app/src/pages/atividades/Atividade.jsx
+++ front/pro-atividade-app/src/pages/atividades/Atividade.jsx
@@ -1,81 +1,144 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+import AtividadeForm from "./AtividadeForm";
+import AtividadeLista from "./AtividadeLista";
+import api from "../../api/atividade";
+import TitlePage from "../../components/TitlePage";
 
-export default function Atividade(props) {
-  function prioridadeLabel(param) {
-    switch (param) {
-      case 'Baixa':
-      case 'Normal':
-      case 'Alta':
-        return param;
-      default:
-        return 'Não definido';
-    }
-  }
+export default function Atividade() {
+  const [showAtividadeModal, setShowAtividadeModal] = useState(false);
+  const [smShowConfirmModal, setSmShowConfirmModal] = useState(false);
 
-  function prioridadeStyle(param, icone) {
-    switch (param) {
-      case 'Baixa':
-        return icone ? 'smile' : 'success';
-      case 'Normal':
-        return icone ? 'meh' : 'dark';
-      case 'Alta':
-        return icone ? 'frown' : 'warning';
-      default:
-        return 'Não definido';
+  const [atividades, setAtividades] = useState([]);
+  const [atividade, setAtividade] = useState({ id: 0 });
+
+  const handleAtiviadeModal = () => setShowAtividadeModal(!showAtividadeModal);
+
+  const handleConfirmModal = (id) => {
+    if (id !== 0 && id !== undefined) {
+      const atividade = atividades.filter((atividade) => atividade.id === id);
+      setAtividade(atividade[0]);
+    } else {
+      setAtividade({ id: 0 });
     }
-  }
+    setSmShowConfirmModal(!smShowConfirmModal);
+  };
+
+  const pegaTodasAtividades = async () => {
+    const response = await api.get("atividade");
+    return response.data;
+  };
+
+  const novaAtividade = () => {
+    setAtividade({ id: 0 });
+    handleAtiviadeModal();
+  };
+
+  useEffect(() => {
+    const getAtividades = async () => {
+      const todasAtividades = await pegaTodasAtividades();
+      if (todasAtividades) setAtividades(todasAtividades);
+    };
+    getAtividades();
+  }, []);
+
+  const addAtividade = async (ativ) => {
+    handleAtiviadeModal();
+    const response = await api.post("atividade", ativ);
+    console.log(response.data);
+    setAtividades([...atividades, response.data]);
+  };
+
+  const cancelarAtividade = () => {
+    setAtividade({ id: 0 });
+    handleAtiviadeModal();
+  };
+
+  const atualizarAtividade = async (ativ) => {
+    handleAtiviadeModal();
+    const response = await api.put(`atividade/${ativ.id}`, ativ);
+    const { id } = response.data;
+    setAtividades(
+      atividades.map((item) => (item.id === id ? response.data : item))
+    );
+    setAtividade({ id: 0 });
+  };
+
+  const deletarAtividade = async (id) => {
+    handleConfirmModal(0);
+    if (await api.delete(`atividade/${id}`)) {
+      const atividadesFiltradas = atividades.filter(
+        (atividade) => atividade.id !== id
+      );
+      setAtividades([...atividadesFiltradas]);
+    }
+  };
+
+  const pegarAtividade = (id) => {
+    const atividade = atividades.filter((atividade) => atividade.id === id);
+    setAtividade(atividade[0]);
+    handleAtiviadeModal();
+  };
 
   return (
-    <div
-      className={
-        'card mb-2 shadow-sm border-' +
-        prioridadeStyle(props.ativ.prioridade)
-      }
-    >
-      <div className='card-body'>
-        <div className='d-flex justify-content-between'>
-          <h5 className='card-title'>
-            <span className='badge bg-secondary me-1'>
-              {props.ativ.id}
-            </span>
-            - {props.ativ.titulo}
-          </h5>
-          <h6>
-            Prioridade:
-            <span
-              className={
-                'ms-1 text-' +
-                prioridadeStyle(props.ativ.prioridade)
-              }
-            >
-              <i
-                className={
-                  'me-1 far fa-' +
-                  prioridadeStyle(props.ativ.prioridade, true)
-                }
-              ></i>
-              {prioridadeLabel(props.ativ.prioridade)}
-            </span>
-          </h6>
-        </div>
-        <p className='card-text'>{props.ativ.descricao}</p>
-        <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+    <>
+      <TitlePage
+        title={"Atividade " + (atividade.id !== 0 ? atividade.id : "")}
+      >
+        <Button variant="outline-secondary" onClick={novaAtividade}>
+          <i className="fas fa-plus"></i>
+        </Button>
+      </TitlePage>
+
+      <AtividadeLista
+        atividades={atividades}
+        pegarAtividade={pegarAtividade}
+        handleConfirmModal={handleConfirmModal}
+      />
+
+      <Modal show={showAtividadeModal} onHide={handleAtiviadeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Atividade {atividade.id !== 0 ? atividade.id : ""}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AtividadeForm
+            addAtividade={addAtividade}
+            cancelarAtividade={cancelarAtividade}
+            atualizarAtividade={atualizarAtividade}
+            ativSelecionada={atividade}
+            atividades={atividades}
+          />
+        </Modal.Body>
+      </Modal>
+
+      <Modal size="sm" show={smShowConfirmModal} onHide={handleConfirmModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Excluindo Atividade {atividade.id !== 0 ? atividade.id : ""}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Tem certeza que deseja Excluir a Atividade {atividade.id}
+        </Modal.Body>
+        <Modal.Footer className="d-flex justify-content-between">
           <button
-            className='btn btn-sm btn-outline-primary me-2'
-            onClick={() => props.pegarAtividade(props.ativ.id)}
+            className="btn btn-outline-success me-2"
+            onClick={() => deletarAtividade(atividade.id)}
           >
-            <i className='fas fa-pen me-2'></i>
-            Editar
+            <i className="fas fa-check me-2"></i>
+            Sim
           </button>
           <button
-            className='btn btn-sm btn-outline-danger'
-            onClick={() => props.handleConfirmModal(props.ativ.id)}
+            className="btn btn-danger me-2"
+            onClick={() => handleConfirmModal(0)}
           >
-            <i className='fas fa-trash me-2'></i>
-            Deletar
+            <i className="fas fa-times me-2"></i>
+            Não
           </button>
-        </div>
-      </div>
-    </div>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
